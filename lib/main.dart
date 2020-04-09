50eee6271993f0dcd63bc8473e1f88e67d2b4fb3
architjain-12 <architjain08@gmail.com>
2020-04-09 20:54:22 +0530

    Second version

--- lib/main.dart
+++ lib/main.dart
@@ -28,30 +28,107 @@ class _DicePageState extends State<DicePage> {
   int rdice = 1;
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  ldice = Random().nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dice$ldice.png'),
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Text(
+                      'Player 1',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(25.0),
+                    child: Text(
+                      'Player 2',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rdice = Random().nextInt(6) + 1;
-                });
-              },
-              child: Image.asset('images/dice$rdice.png'),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        ldice = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$ldice.png'),
+                  ),
+                ),
+                SizedBox(
+                  width: 10.0,
+                  height: 150.0,
+                  child: VerticalDivider(
+                    color: Colors.white,
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        rdice = Random().nextInt(6) + 1;
+                      });
+                    },
+                    child: Image.asset('images/dice$rdice.png'),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(15.0 , 20.0 ,30 ,20),
+                      child: Text('Player1 got $ldice',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(32.0 , 20.0 ,20 ,20),
+                      child: Text('Player2 got $rdice',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
